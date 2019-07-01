9c416728d37008e723bd83f8f24c13b2117fb1e7
vinuthabr <42146405+vinuthabr@users.noreply.github.com>
2019-07-01 20:18:12 +0530

    Add files via upload

---
+++ form.js
@@ -0,0 +1,5 @@
+var Fname=prompt("What is your First name?");
+var Lname=prompt("What is your Last name?");
+var Age=prompt("What is your Age?");
+console.log("Your Full Name is " + Fname +" "+ Lname)
+console.log("You are "+ Age +" years old")
