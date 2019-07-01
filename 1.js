36493b11b0ab557f8d77aaa4b0cf74fedffc4d43
vinuthabr <42146405+vinuthabr@users.noreply.github.com>
2019-07-01 20:17:19 +0530

    Delete 1.js

--- 1.js
+++
@@ -1,5 +0,0 @@
-var Fname=prompt("What is your First name?");
-var Lname=prompt("What is your Last name?");
-var Age=prompt("What is your Age?");
-console.log("Your Full Name is " + Fname +" "+ Lname)
-console.log("You are "+ Age +" years old")
